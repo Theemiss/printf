ca520c7b906e37b0ae644b878d42599f9c26d454
benhassenwael <greenw2012@gmail.com>
2020-10-23 18:54:50 +0200

    Fix: reversed print

--- print_binary.c
+++ print_binary.c
@@ -11,14 +11,20 @@
 int print_binary(char *format_spec, va_list ap)
 {
 	unsigned int count = 0, n;
+	int i = 0;
+	char binary[32];
 
 	if (!ap || !format_spec)
 		return (0);
 	n = va_arg(ap, unsigned int);
 	while (n)
 	{
-		_putchar((n % 2) + '0');
+		binary[i++] = '0' + (n % 2);
 		n = n / 2;
+	}
+	while (i >= 0)
+	{
+		_putchar(binary[i--]);
 		count++;
 	}
 	return (count);
