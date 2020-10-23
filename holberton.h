da4289e1221b70663a7db74f058b5989384a588a
AhmedBelhaj <midinfotn401@gmail.com>
2020-10-23 20:33:44 +0100

    printing char update

--- holberton.h
+++ holberton.h
@@ -1,8 +1,10 @@
 #ifndef _FUNCTION_PRINTF_H_
 #define _FUNCTION_PRINTF_H_
 #include <stdarg.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int print_binary(char *, va_list);
+int print_char (va_list ap);
 
 #endif
