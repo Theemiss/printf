7d167fdfcf579b4c0db4250287826b97c593e9c1
benhassenwael <greenw2012@gmail.com>
2020-10-25 11:47:47 +0100

    add modifier_excavators

--- holberton.h
+++ holberton.h
@@ -2,6 +2,27 @@
 #define _FUNCTION_PRINTF_H_
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * struct modifier - mofifier fields collection
+ * @flags: flags field composed of ['0', ' ', '#', '+', '-']
+ * @width: width field, positive number
+ * @precision: precision field positive number not including '.'
+ * or -1 for '*'
+ * @length: length field string composed of ['h', 'l']
+ * @specifier: specifier character can one of
+ * ['c', 's', '%', 'd', 'i', 'b', 'u', 'o', 'x', 'X', 'S', 'p', 'r', 'R']
+ *
+ */
+typedef struct modifier
+{
+	char *flags;
+	int width;
+	int precision;
+	char *length;
+	char specifier;
+} modifier_t;
 
 int _putchar(char c);
 int print_binary(char *, va_list);
@@ -11,5 +32,9 @@ int print_hex(char *, va_list);
 int print_char(va_list ap);
 int print_int(va_list ap);
 int print_decimal(va_list ap);
+char *get_flags(char*, unsigned int*);
+int get_width(char *, unsigned int*);
+int get_precision(char *, unsigned int*);
+char get_specifier(char *, unsigned int*);
 
 #endif
