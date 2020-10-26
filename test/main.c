0a4c3433689f888dad27584815d7fb8880f69b6d
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-26 21:51:57 +0100

    fixed test file

--- test/main.c
+++ test/main.c
@@ -2,17 +2,8 @@
 #include <stdio.h>
 #include "holberton.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
 
-    _printf("Unknown:[%R]\n", "encrypted message");#include <limits.h>
-#include <stdio.h>
-#include "holberton.h"
+
 
 /**
  * main - Entry point
@@ -22,34 +13,23 @@ int main(void)
 int main(void)
 {
     int len;
-    int len2;
     unsigned int ui;
     void *addr;
 
+    _printf("Unknown:[%R]\n", "encrypted message");
     len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
     _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
     _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
     _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
     _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
     _printf("%%\n");
@@ -60,10 +40,7 @@ _printf("minehello%");
 _printf("hello%r\n");
 _printf("mineUnknown:[%r]\n");
 _printf("mineCharacter:[%c]\n", 'H');
-printf("Character:[%c]\n", 'H');
 _printf("Let's try to printf a simple sentence.\n");
-printf("Let's try to printf a simple sentence.\n");
 _printf("String:[%s]\n", "I am a string !");
-printf("String:[%s]\n", "I am a string !");
     return (0);
 }
