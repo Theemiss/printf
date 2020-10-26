2d5f081f5db49418df4e7d401716e57e9f527b8d
ahmed <midinfotn401@gmail.com>
2020-10-26 10:15:44 +0100

    betty done for print_char

--- print_char.c
+++ print_char.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
 /**
  * print_char - print a single char
- * @ap: va_list pointer
+ * @ap: va_list pointer containg a char
+ * @modif: struct modifier containig modifier fields
  * Return: one char count
  */
 int print_char (modifier_t *modif, va_list ap)
 {
 	char ch;
+
 	if (!ap || !modif)
 		return (0);
 
 	ch = (char)va_arg(ap, int);
-		_putchar(ch);
+	_putchar(ch);
 
-		return (1);
+	return (1);
 }
