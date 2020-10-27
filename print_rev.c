4c8a05cf79e2a6cd31c34cb8b8a498986120e68f
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-27 23:33:37 +0100

    update reverse

--- print_rev.c
+++ print_rev.c
@@ -1,21 +1,4 @@
 #include "holberton.h"
-char *reverse (char *s)
-{
-	int count, end, begin;
-	char *r = malloc (_strlen(s) * sizeof(char) + 1);
-	while (s[count] != '\0')
-      count++;
-
-   end = count - 1;
-
-   for (begin = 0; begin < count; begin++) {
-      r[begin] = s[end];
-      end--;
-   }
-
-   r[begin] = '\0';
-	return(r);
-}
 /**
  * print_rev - print a string in reverse
  * @ap:handle %r
@@ -24,19 +7,21 @@ char *reverse (char *s)
  */
 char *print_rev(modifier_t *modif, va_list ap)
 {
-			char *str, *res;
+	int i;
+	int length;
+	char *str, *ret;
+	
 	if (!ap || !modif)
 		return (0);
 	str = va_arg(ap, char *);
-	if (str == NULL)
-	{
-		res = malloc(sizeof(char) * 7);
-		res = _strcpy(res, "(null)");
-		return (res);
-	}
-	if (str[0] == '\0')
+	length = _strlen(str);
+	ret = malloc(length + 1);
+	if (!ret)
 		return (NULL);
-	malloc(sizeof(char) * _strlen(str));
-	res = reverse(str);
-	return (res);
+
+	for (i = 0; i < length; i++)
+		ret[i] = str[length - i - 1];
+	ret[i] = '\0';
+
+	return (ret);
 }
