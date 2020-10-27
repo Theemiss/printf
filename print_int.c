30799ace2490f0d41c7aa168d3b6e062060a88be
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-27 19:39:50 +0100

    deleted function

--- print_int.c
+++ print_int.c
@@ -1,47 +1,4 @@
 #include "holberton.h"
-void array_rev(char *arr, int len)
-{
-	int i;
-	char tmp;
-
-	for (i = 0; i < (len / 2); i++)
-	{
-		tmp = arr[i];
-		arr[i] = arr[(len - 1) - i];
-		arr[(len - 1) - i] = tmp;
-	}
-}
-int int_len(int num)
-{
-	int len = 0;
-
-	while (num != 0)
-	{
-		len++;
-		num /= 10;
-	}
-	return (len);
-}
-char *ito( int n)
-{
-	int len = 0, i = 0;
-	char *str;
-
-	len = int_len(n);
-	str = malloc(len + 1);
-	if (!str)
-		return (NULL);
-	while (n / 10)
-	{
-		str[i] = (n % 10) + '0';
-		n /= 10;
-		i++;
-	}
-	str[i] = (n % 10) + '0';
-	array_rev(str, len);
-	str[i + 1] = '\0';
-	return (str);
-}
 /**
  * print_int - print integer
  * @ap:va_list pointer for integer handle %i
