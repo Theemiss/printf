b93f6ecafb71d0a8a80d1500ec8312da86a35e9e
Theemiss <58787184+Theemiss@users.noreply.github.com>
2020-10-27 23:27:20 +0100

    updated rot

--- print_rot.c
+++ print_rot.c
@@ -1,10 +1,22 @@
 #include "holberton.h"
-#include "holberton.h"
+/**
+ * _isalpha - checks if character is alpha or not
+ * @c: character to check
+ *
+ * Return: 1 if is alpha, 0 otherwise
+ */
+int _isalpha(char c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
 /**
  * _strlen - lenght of string
  * @s:char
  * Return:int
  */
+
 int _strlen(char *s)
 {
 	int i;
@@ -16,37 +28,6 @@ int _strlen(char *s)
 	return (i);
 }
 /**
- * rot13 - encodes a string into rot13
- * @str: string to encode
- *
- * Return: address of s
- */
-char *rot13(char *str)
-{
-	int i;
-	int j;
-	int l = _strlen(str);
-	char *s;
-	char *alph = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *shift = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
-	s = malloc(l + 1);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		for (j = 0; j < 52; j++)
-		{
-			if (str[i] == alph[j])
-			{
-				s[i] = shift[j];
-				break;
-			}
-		}
-		if (j == 52)
-			s[i] = str[i];
-	}
-	return (s);
-}
-/**
  * print_rot - encodes a string into rot13
  * @ap: arg to display handle %R
 * @modif:struct modifier containig modifier fields
@@ -54,19 +35,40 @@ char *rot13(char *str)
  */
 char *print_rot(modifier_t *modif, va_list ap)
 {
-	char *str, *res;
+int index, length;
+	unsigned char temp_c;
+	char is_upper;
+	char *str, *ret;
+
 	if (!ap || !modif)
-		return (0); 
+		return (0);
 	str = va_arg(ap, char *);
-	if (str == NULL)
-	{
-		res = malloc(sizeof(char) * 7);
-		res = _strcpy(res, "(null)");
-		return (res);
-	}
-	if (str[0] == '\0')
+	length = _strlen(str);
+	ret = malloc(length + 1);
+	if (!ret)
 		return (NULL);
-	malloc (sizeof(char)* _strlen(str));
-	res =rot13(str);
-	return (res);
-}
+	index = 0;
+	while (str[index])
+	{
+		temp_c = (unsigned char)str[index];
+		if (_isalpha(temp_c))
+		{
+			is_upper = (temp_c >= 'A' && temp_c <= 'Z');
+			temp_c += 13;
+			if (is_upper)
+			{
+				if (temp_c > 'Z')
+					temp_c -= 26;
+			}
+			else
+			{
+				if (temp_c > 'z')
+					temp_c -= 26;
+			}
+		}
+		ret[index] = (char)temp_c;
+		index++;
+	}
+	ret[length] = '\0';
+	return (ret);
+	}
