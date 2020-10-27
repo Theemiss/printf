26059bc6f82211a0d7f77db94ff42a84af8e1ea3
benhassenwael <greenw2012@gmail.com>
2020-10-27 12:10:49 +0100

    fix: print 'x(null)' when no arg found

--- _printf.c
+++ _printf.c
@@ -3,12 +3,16 @@
 #include <stdio.h>
 
 /**
+ * buffer_print - print given buffer to stdout
+ * @buffer: buffer to print
+ * @nbytes: number of bytes to print
  *
+ * Return: nbytes
  */
-int buffer_print(char buffer[], unsigned int pos)
+int buffer_print(char buffer[], unsigned int nbytes)
 {
-	write(1, buffer, pos);
-	return (pos);
+	write(1, buffer, nbytes);
+	return (nbytes);
 }
 
 /**
